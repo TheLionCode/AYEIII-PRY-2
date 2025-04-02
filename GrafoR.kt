27db7c4b1a616c089668ae22121ae7a8bdbda3c2
thelioncode <gi.carballo10@gmail.com>
2025-04-01 22:51:02 -0400

    Algorithms refactor used for the exam

--- GrafoR.kt
+++ GrafoR.kt
@@ -9,28 +9,10 @@ fun printLadosGrafo(g: Grafo) {
 }
 
 fun main(args: Array<String>) {
-    val archivo1 = 2
-    val archivo2 = 3
-    var g = GrafoNoDirigido(10)
-    g.agregarArista(Arista(1, 2))
-    g.agregarArista(Arista(3, 2))
-    g.agregarArista(Arista(7, 2))
-    g.agregarArista(Arista(7, 4))
-    g.agregarArista(Arista(5, 7))
-    g.agregarArista(Arista(5, 6))
-    g.agregarArista(Arista(8, 9))
-    println("g: ${g}")
-    val arista1 = AristaCosto(1, 2, 3.9)
-    val arista2 = AristaCosto(1, 2, 4.9)
-    println("arista1: ${arista1}")
-    println("arista2: ${arista2}")
-    println("comparacion a1 a2 : ${arista1.compareTo(arista2)}")
+    val archivo1 = "grafo3.txt"
+
     println("Grafo desde archivo")
-    g = GrafoNoDirigido(archivo1)
-    printLadosGrafo(g)
-    println("Grafo desde archivo con costo")
-    val g1 = GrafoNoDirigidoCosto(archivo2)
-    printLadosGrafo(g1) 
-    val gd = GrafoDirigidoCosto(archivo2)
-    printLadosGrafo(gd) 
+    val g1 = GrafoDirigido(archivo1)
+    println(g1.dfs_rw())
+
 }
--- libGrafoKt/GrafoDirigidoCosto.kt
+++ libGrafoKt/GrafoDirigidoCosto.kt
@@ -1,6 +1,9 @@
 import java.io.File
 import java.util.Arrays
 
+import java.util.Stack
+import kotlin.math.abs
+
 //GRAFO_DIRIGIDO_COSTO
 public class GrafoDirigidoCosto : Grafo {
     var numeroDeLados = 0
@@ -50,6 +53,73 @@ public class GrafoDirigidoCosto : Grafo {
 		}
 	    }
 	}
+    }
+
+    fun dfs_rw(): List<Int> {
+        val explored = mutableSetOf<Int>()
+        val result = linkedSetOf<Int>()
+        val vertices = (1..numDeVertices).toList() // Vertices are 1-based in the input file
+    
+        fun explore(vertex: Int) {
+            explored += vertex
+            for (arc in adyacentes(vertex)) {
+                val successor = arc.sumidero()
+                if (successor !in explored) {
+                    explore(successor)
+                } else if (successor !in result) {
+                    error("Graph contains a cycle, topological sort not possible!")
+                }
+            }
+            result += vertex
+        }
+    
+        for (vertex in vertices) {
+            if (vertex !in explored) {
+                explore(vertex)
+            }
+        }
+    
+        return result.reversed()
+    }
+    
+    fun kahn(): List<Int> {
+        // Initialize in-degree map
+        val inDegree = mutableMapOf<Int, Int>().apply {
+            for (v in 1..numDeVertices) {
+                put(v, 0)
+            }
+            for (arc in this@GrafoDirigidoCosto) {
+                put(arc.sumidero(), getOrDefault(arc.sumidero(), 0) + 1)
+            }
+        }
+    
+        // Initialize queue with vertices of 0 in-degree
+        val queue = ArrayDeque<Int>().apply {
+            addAll(inDegree.filter { it.value == 0 }.keys)
+        }
+    
+        val result = mutableListOf<Int>()
+        var visitedCount = 0
+    
+        while (queue.isNotEmpty()) {
+            val u = queue.removeFirst()
+            result.add(u)
+            visitedCount++
+    
+            for (arc in adyacentes(u)) {
+                val v = arc.sumidero()
+                inDegree[v] = inDegree[v]!! - 1
+                if (inDegree[v] == 0) {
+                    queue.add(v)
+                }
+            }
+        }
+    
+        if (visitedCount != numDeVertices) {
+            throw IllegalStateException("Graph contains at least one cycle!")
+        }
+    
+        return result
     }
 
     // Agrega un lado al digrafo
